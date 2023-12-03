200513d503a10fc150b854b3701324a493fc3f26
mmadamm <122172059+mmadamm@users.noreply.github.com>
2023-12-04 01:14:55 +0330

    create app and models

---
+++ shop/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name'])
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=250)
+    description = models.TextField(max_length=1200)
+    inventory = models.PositiveIntegerField(default=0)
+    price = models.PositiveIntegerField(default=0)
+    off = models.PositiveIntegerField(default=0)
+    new_price = models.PositiveIntegerField(default=0)  # price after off
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
+    file = models.ImageField(upload_to="product_images/%Y/%m/%d")
+    title = models.CharField(max_length=250, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['-created'])
+        ]
+
+
+class ProductFeature(models.Model):
+    name = models.CharField(max_length=255)
+    value = models.CharField(max_length=255)
+    product = models.ForeignKey(Product, related_name='features', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name + ":" + self.value
