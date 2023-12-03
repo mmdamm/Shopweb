3ef05611868e83b370a39cb7fc0586fa9b9b25b8
mmadamm <122172059+mmadamm@users.noreply.github.com>
2023-12-04 01:55:57 +0330

    admin and signals

--- Shopweb/settings.py
+++ Shopweb/settings.py
@@ -103,6 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
