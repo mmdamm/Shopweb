c536b4546ffebc4da20ec7235aaab66bbfbaedc5
mmadamm <122172059+mmadamm@users.noreply.github.com>
2024-01-31 18:57:54 +0330

    cart app

---
+++ cart/cart.py
@@ -0,0 +1,60 @@
+from shop.models import Product
+
+
+class Cart:
+    def __init__(self, request):
+        self.session = request.session
+        cart = self.session.get('cart')
+        if not cart:
+            cart = self.session['cart'] = {}
+        self.cart = cart
+
+    def add(self, product):
+        product_id = str(Product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 1, 'price': Product.new_price, 'weight': Product.weight}
+        else:
+            if self.cart[product_id]['quantity'] < product.inventory:
+                self.cart[product_id]['quantity'] += 1
+        self.save()
+
+    def decrease(self, product):
+        product_id = str(product.id)
+        if self.cart[product_id]['quantity'] > 0:
+            self.cart[product_id]['quantity'] -= 1
+        self.save()
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        cart_dict = self.cart.copy()
+        for product in products:
+            cart_dict[str(product.id)]['product'] = product
+        for item in cart_dict.values():
+            yield item
+
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def get_total_price(self):
+        return sum(item['price'] * item['quantity'] for item in self.cart.values())
+
+    def get_post_price(self):
+        weight = sum(item['quantity'] * item['weight'] for item in self.cart.values())
+        if weight > 1000:
+            return 60000
+        else:
+            return 100000
+
+    def clear(self):
+        del self.session
+        self.save()
+
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+        self.save()
+
+    def save(self):
+        self.session.modified = True
